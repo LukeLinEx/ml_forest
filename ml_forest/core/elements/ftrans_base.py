10594b67ceca6248eb65e2f978d8b3f1be20391d
LukeLinEx <lukelinex@gmail.com>
2018-06-22 18:26:46 -0400

    implement and test some FTransformer

--- ml_forest/core/elements/ftrans_base.py
+++ ml_forest/core/elements/ftrans_base.py
@@ -36,3 +36,26 @@ class FTransform(Base):
     @property
     def tuning(self):
         return self.__tuning
+
+
+class SklearnModel(FTransform):
+    def __init__(self, model_type, **kwargs):
+        super(SklearnModel, self).__init__(**kwargs)
+        self.__model_type = model_type
+
+        self.__essentials = {}
+
+    def fit_singleton(self, x, y, new_x):
+        model = self.__model_type()
+        for key in model.get_params():
+            if key in self.__essentials:
+                model.set_params(**{key: self.essentials[key]})
+        model.fit(x, y)
+
+        return model, model.predict(new_x)
+
+    def transform_singleton(self, model, new_x):
+        if "predict_proba" in self.essentials and self.essentials["predict_proba"]:
+            return model.predict_proba(new_x)
+        else:
+            return model.predict(new_x)
---
+++ ml_forest/feature_transformations/linear_family/lasso.py
@@ -0,0 +1,35 @@
+from sklearn.linear_model import Lasso
+
+from ml_forest.core.elements.ftrans_base import SklearnModel
+
+
+class GenerateLasso(SklearnModel):
+    def __init__(
+            self, alpha=1.0, fit_intercept=True, max_iter=1000, normalize=True, precompute=False, positive=False,
+            random_state=None, selection='cyclic', tol=0.0001, warm_start=False, **kwargs
+    ):
+        super(GenerateLasso, self).__init__(model_type=Lasso, rise=1, **kwargs)
+
+        # get essentials
+        essential_keys = {
+            "normalize", "warm_start", "selection", "fit_intercept", "positive",
+            "max_iter", "precompute", "random_state", "tol", "alpha"
+        }
+        self.__essentials = {}
+        for key in essential_keys:
+            self.__essentials[key] = locals()[key]
+
+
+
+
+if __name__ == "__main__":
+    lasso = GenerateLasso()
+    from sklearn.datasets import load_iris
+
+    iris = load_iris()
+    X = iris.data
+    y = iris.target.reshape(-1, 1)
+
+    mod, v1 = lasso.fit_singleton(X, y, X)
+    v2 = lasso.transform_singleton(mod, X)
+    print((v1 == v2).all())
