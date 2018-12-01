e53b7030b4e68b9b8d45d65b4c9c65915f040211
Luke Lin <lukelinex@gmail.com>
2018-12-01 18:43:19 -0500

    make evaluator a class

--- performance/evaluators.py
+++ performance/evaluators.py
@@ -11,12 +11,52 @@ def ravel_single_variable(x):
 from sklearn.metrics import mean_squared_error
 
 
-def rmse(fval, lval):
-    f = ravel_single_variable(fval)
-    l = ravel_single_variable(lval)
+class Evaluator(object):
+    def __call__(self, fval, lval):
+        raise NotImplementedError
 
-    rmse_ = np.sqrt(
-        mean_squared_error(f, l)
-    )
 
-    return rmse_
+class RMSE(Evaluator):
+    def __call__(self, fval, lval):
+        f = ravel_single_variable(fval)
+        l = ravel_single_variable(lval)
+
+        rmse_ = np.sqrt(
+            mean_squared_error(f, l)
+        )
+
+        return rmse_
+
+class LRMSE_pred_been_trans(Evaluator):
+    def __init__(self, shift=0):
+        self.shift = shift
+
+    def __call__(self, fval, lval):
+        f = ravel_single_variable(fval)
+        lval = np.log(lval + self.shift)
+        l = ravel_single_variable(lval)
+
+        rmse_ = np.sqrt(mean_squared_error(f, l))
+
+        return rmse_
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    a = np.array([1,2,3])
+    b = np.array([1,2,3])
+    c = np.array([3,2,1])
+
+    evaluator = RMSE()
+    print(type(evaluator).__name__)
+    print(evaluator(a, b))
+    print(evaluator(a, c))
+
+    print("\n")
+
+    shift = 1
+    evaluator = LRMSE_pred_been_trans(shift)
+    print(type(evaluator).__name__)
+    print(evaluator(np.log(a+shift), b))
+    print(evaluator(a, c))
